3ef88bf28ea70a332d22ca48b893682e72fc0081
MSmaga <msmaga@protonmail.com>
2022-04-06 06:22:44 +0300

    Fixed the algorithm.

--- Task3_RemoveDublicatesSortedList/Program.cs
+++ Task3_RemoveDublicatesSortedList/Program.cs
@@ -1,56 +1,64 @@
 ﻿class Program
 {
     /// <summary>
-    /// Adds an element into the array.
-    /// </summary>
-    /// <param name="array"></param>
-    /// <param name="element"></param>
-    static void AddElement(ref int[] array, int element)
-    {
-        Array.Resize(ref array, array.Length + 1);
-        array[^1] = element;
-    }
-
-    /// <summary>
     /// Deletes duplicate elements of the array.
     /// </summary>
     /// <param name="array"></param>
     /// <returns></returns>
     static int[] DelDuplicates(int[] array)
     {
-        int[] _buffer = new int[0];
+        int[] _buffer = new int[array.Length];
 
         int i = 0;
+        int k = 0;
+
         while (i < array.Length - 1)
         {
             if (array[i] != array[i + 1])
-                AddElement(ref _buffer, array[i]);
+            {
+                _buffer[k] = array[i];
+                k++;
+            }
 
             if (i == array.Length - 2)
-                AddElement(ref _buffer, array[i + 1]);
+            {
+                _buffer[k] = array[i + 1];
+                k++;
+            }
+
             i++;
         }
+
+        int j = 0;
+
+        while (j < _buffer.Length - 1 && _buffer[j] != _buffer[j + 1])
+        {
+            j++;
+        }
+
+        Array.Resize(ref _buffer, j);
+
         return _buffer;
     }
 
-    static void Main()
-    {
-        #region Task
-        // 3.Write a program to remove duplicates from a sorted int[]
-        // INPUT: int[] { 1,2,3,4,4,56}
-        // OUTPUT: int[] { 1,2,3,4,56}
-        // You are not allowed to modify an existing array.
-        // You are not allowed to use LINQ.
-        #endregion
+static void Main()
+{
+    #region Task
+    // 3.Write a program to remove duplicates from a sorted int[]
+    // INPUT: int[] { 1,2,3,4,4,56}
+    // OUTPUT: int[] { 1,2,3,4,56}
+    // You are not allowed to modify an existing array.
+    // You are not allowed to use LINQ.
+    #endregion
 
-        int[] array = new int[] { 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6 };
+    int[] array = new int[] { 1, 3, 3, 3, 32, 5, 132, 132, 66, 66, 622, 622, 622 };
 
-        int[] newarray = DelDuplicates(array);
+    int[] newarray = DelDuplicates(array);
 
-        foreach (int element in array) Console.Write(element + " ");
-        Console.WriteLine();
+    foreach (int element in array) Console.Write(element + " ");
+    Console.WriteLine();
 
-        foreach (int element in newarray) Console.Write(element + " ");
-        Console.ReadLine();
-    }
+    foreach (int element in newarray) Console.Write(element + " ");
+    Console.ReadLine();
+}
 }
